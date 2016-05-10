2bf8655d211fabde6bbd5b030245dc0371e54946
gardian12 <gardian12@d030d3d0-4d34-45fe-80b7-4d6c39212f34>
2016-05-10 23:23:04 +0000

    stable version 1.0.0.0

---
+++ SAPLogonHandler.cs
@@ -0,0 +1,84 @@
+﻿/*
+  Copyright (C) 2016 Marko Graf
+*/
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+using KeePassLib.Security;
+
+
+
+namespace KeeSAPLogon
+{
+    public class SAPLogonHandler
+    {
+        public static string SAPGUIShortCutEXE = "sapshcut.exe";
+
+        private static string m_sapguiPath = "";
+
+
+        public static string SAPGUIPath
+        {
+            get { return m_sapguiPath; }
+            set { m_sapguiPath = value; }
+        }
+
+
+        public static bool DoLogon(LogonColumn logonPoint, ProtectedString user, ProtectedString password)
+        {
+            if (ValidateSAPGUIPath(m_sapguiPath) && logonPoint.IsValid())
+            {
+                //Example:
+                //
+                // "C:\Program Files (x86)\SAP\FrontEnd\SAPgui\sapshcut" -system=ABC -client=010 -user=logonname -pw=pass -language=DE -maxgui -command=SE10
+                //
+
+                string strArgs = "-maxgui";
+
+                Dictionary<string, string> args = new Dictionary<string, string>();
+                args.Add("-system", logonPoint.SAPID);
+                args.Add("-client", logonPoint.SAPClient);
+                args.Add("-user", user.ReadString());
+                args.Add("-pw", password.ReadString());
+                args.Add("-language", logonPoint.SAPLanguage);
+                args.Add("-command", logonPoint.SAPTransaction);
+
+                foreach (string key in args.Keys)
+                {
+                    string value;
+                    args.TryGetValue(key, out value);
+                    strArgs = strArgs + " ";
+                    strArgs = strArgs + key + "=" + value;
+                }
+
+                string fileLoc = Path.Combine(m_sapguiPath, SAPLogonHandler.SAPGUIShortCutEXE);
+                FileInfo fileInfo = new FileInfo(fileLoc);
+                ProcessStartInfo info = new ProcessStartInfo(fileInfo.FullName);
+                info.Arguments = strArgs;
+                info.CreateNoWindow = false;
+                info.UseShellExecute = true;
+                info.ErrorDialog = true;
+                info.RedirectStandardInput = false;
+                info.RedirectStandardOutput = false;
+
+                Process process = Process.Start(info);
+
+                return !(process.HasExited);
+            }
+
+            return false;
+        }
+
+
+        public static bool ValidateSAPGUIPath(string path)
+        {
+            //Validating if 'sapshcut.exe' located by given path
+            string fileLoc = Path.Combine(path, SAPLogonHandler.SAPGUIShortCutEXE);
+            FileInfo fileInfo = new FileInfo(fileLoc);
+            return (fileInfo.Exists);
+        }
+
+    }
+}
---
+++ SAPLogonOpt.cs
@@ -0,0 +1,89 @@
+﻿/*
+  Copyright (C) 2016 Marko Graf
+*/
+
+using System;
+
+using KeePass.App.Configuration;
+
+
+
+namespace KeeSAPLogon
+{
+    public class SAPLogonOpt : IDisposable
+    {
+        private const string flagDisSystemID = KeeSAPLogonExt.PlugInName + "_" + "DisSystemID";
+        private const string flagDisClient = KeeSAPLogonExt.PlugInName + "_" + "DisClient";
+        private const string flagDisLanguage = KeeSAPLogonExt.PlugInName + "_" + "DisLanguage";
+        private const string flagDisTx = KeeSAPLogonExt.PlugInName + "_" + "DisTx";
+        private const string flagDefaultLng = KeeSAPLogonExt.PlugInName + "_" + "DefaultLng";
+        private const string flagDefaultTx = KeeSAPLogonExt.PlugInName + "_" + "DefaultTx";
+        private const string flagSAPGUIPath = KeeSAPLogonExt.PlugInName + "_" + "SAPGUIPath";
+
+        readonly AceCustomConfig m_config = null;
+
+
+
+        public SAPLogonOpt(AceCustomConfig config)
+        {
+            m_config = config;
+        }
+
+
+        public bool DisSystemID
+        {
+            get { return m_config.GetBool(flagDisSystemID, true); }
+            set { m_config.SetBool(flagDisSystemID, value); }
+        }
+
+        public bool DisClient
+        {
+            get { return m_config.GetBool(flagDisClient, true); }
+            set { m_config.SetBool(flagDisClient, value); }
+        }
+
+        public bool DisLanguage
+        {
+            get { return m_config.GetBool(flagDisLanguage, false); }
+            set { m_config.SetBool(flagDisLanguage, value); }
+        }
+
+        public bool DisTx
+        {
+            get { return m_config.GetBool(flagDisTx, false); }
+            set { m_config.SetBool(flagDisTx, value); }
+        }
+
+        public string DefaultLng
+        {
+            get { return m_config.GetString(flagDefaultLng, ""); }
+            set { m_config.SetString(flagDefaultLng, value); }
+        }
+
+        public string DefaultTx
+        {
+            get { return m_config.GetString(flagDefaultTx, ""); }
+            set { m_config.SetString(flagDefaultTx, value); }
+        }
+
+        public string SAPGUIPath
+        {
+            get { return m_config.GetString(flagSAPGUIPath, ""); }
+            set { m_config.SetString(flagSAPGUIPath, value); }
+        }
+
+
+        //---------------------------------------------------------------------------------------------------
+        // Interface Implementation: IDisposable
+        //---------------------------------------------------------------------------------------------------
+        #region IDisposable implementation
+
+        public void Dispose()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
